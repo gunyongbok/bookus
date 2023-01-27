9550bc9df541fb542ab8a4eda3c500969af47f23
gunyongbok <qhrrjsdyd123@gmail.com>
2023-01-27 11:45:49 +0900

    feat: 검색창 서비스 구현 완료

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Li = styled.li`
+    list-style: none;
+`;
+
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    width: 110px;
+    height: 250px;
+    position: relative;
+    top: -20px;
+`;
+
+const ImgBox = styled.div`
+    height: 60%;
+    width: 110px;
+    border-bottom: 1px solid #bcbaba;
+`;
 
 const Item = (props) => {
     return (
-        <li>
-            <h3>{props.title}</h3>
-        </li>
+        <Li>
+            <Container>
+                <ImgBox>
+                    <img style={{ height: '140px', width: '110px' }} src={props.thumbnail} alt={props.thumbnail} />
+                </ImgBox>
+                <h3 style={{ fontSize: '12px', fontWeight: '500', height: '15%', width: '100%', display: 'flex', justifyContent: 'flex-start' }}>{props.title}</h3>
+                <h4 style={{ fontSize: '10px', fontWeight: '300', width: '100%', display: 'flex', justifyContent: 'flex-start', position: 'absolute', bottom: '0' }}>{props.authors}</h4>
+            </Container>
+        </Li>
     );
 };
 
--- src/router/Search.jsx
+++ src/router/Search.jsx
@@ -1,13 +1,134 @@
 import React, { useState } from 'react';
 import { useEffect } from 'react';
+import styled from 'styled-components';
 import { bookSearch } from '../components/api';
 import Item from '../components/Item';
+import { CgProfile } from 'react-icons/cg';
+import { AiOutlineLeft, AiOutlineRight, AiOutlineSearch } from 'react-icons/ai';
+
+const TopContainer = styled.div`
+    display: flex;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Container = styled.div`
+    background-color: #e4e4e4;
+    width: 1200px;
+    height: 670px;
+    position: relative;
+`;
+
+const Header = styled.header`
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-size: 35px;
+    font-weight: 250;
+    position: absolute;
+    top: 70px;
+    right: 15px;
+`;
+
+const Profile = styled.div`
+    font-size: 30px;
+    position: absolute;
+    right: 50px;
+    top: 50px;
+`;
+
+const Main = styled.div`
+    background-color: white;
+    width: 850px;
+    height: 400px;
+    position: absolute;
+    left: 180px;
+    top: 170px;
+`;
+
+const SearchBox = styled.input`
+    width: 380px;
+    height: 30px;
+    position: relative;
+    top: 50px;
+    left: 230px;
+    padding-left: 20px;
+    border-radius: 20px;
+    &:focus {
+        outline: none;
+        background-color: white;
+    }
+`;
+
+const SearchIcon = styled.button`
+    display: flex;
+    font-size: 25px;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    left: 590px;
+    top: 22px;
+    border: transparent;
+    background-color: transparent;
+    border-left: 1px solid black;
+    width: 40px;
+    height: 20px;
+`;
+
+const SearchContainer = styled.div`
+    position: relative;
+    margin: 0;
+    top: 70px;
+    left: 160px;
+    width: 550px;
+    height: 250px;
+    border-top: 1px solid #bcbaba;
+    border-bottom: 1px solid #bcbaba;
+`;
+
+const Ul = styled.ul`
+    display: flex;
+    justify-content: space-between;
+    position: relative;
+    left: -15px;
+`;
+
+const LeftBtn = styled.button`
+    border: none;
+    background-color: transparent;
+    font-size: 23px;
+    position: absolute;
+    top: 230px;
+    left: 125px;
+`;
+
+const RightBtn = styled.button`
+    border: none;
+    background-color: transparent;
+    font-size: 23px;
+    position: absolute;
+    top: 230px;
+    right: 105px;
+`;
+
+const XBtn = styled.div`
+    font-size: 25px;
+    font-weight: 350;
+    color: #bcbaba;
+    position: absolute;
+    right: 40px;
+    top: 30px;
+`;
 
 const Search = (props) => {
     const [books, setBooks] = useState([]);
     const [text, setText] = useState('');
     const [query, setQuery] = useState('');
     const [page, setPage] = useState(1);
+
+    console.log(page);
 
     const upPage = () => {
         bookSearchHttpHandler(query, true);
@@ -57,28 +178,47 @@ const Search = (props) => {
 
     return (
         <>
-            <input
-                type="search"
-                placeholder="검색어를 입력하시오"
-                name="query"
-                onKeyDown={onEnter} //enter
-                onChange={onTextUpdate} //change
-                value={text}
-            />
-            <ul>
-                {books?.map((a, i) => (
-                    <Item key={i} title={a.title} />
-                ))}
-            </ul>
-            <button onClick={upPage}>+</button>
-
-            {page <= 1 ? (
-                <button disabled="disabled" onClick={downPage}>
-                    -
-                </button>
-            ) : (
-                <button onClick={downPage}>-</button>
-            )}
+            <TopContainer>
+                <Container>
+                    <Header>BOOKUS</Header>
+                    <Profile>
+                        <CgProfile />
+                    </Profile>
+                    <Main>
+                        <XBtn>x</XBtn>
+                        <SearchBox
+                            type="text"
+                            placeholder="검색어를 입력하시오"
+                            name="query"
+                            onKeyDown={onEnter} //enter
+                            onChange={onTextUpdate} //change
+                            value={text}
+                        />
+                        <SearchIcon onClick={onEnter}>
+                            <AiOutlineSearch />
+                        </SearchIcon>
+                        {page <= 1 ? (
+                            <LeftBtn disabled="disabled">
+                                <AiOutlineLeft />
+                            </LeftBtn>
+                        ) : (
+                            <LeftBtn onClick={downPage}>
+                                <AiOutlineLeft />
+                            </LeftBtn>
+                        )}
+                        <RightBtn onClick={upPage}>
+                            <AiOutlineRight />
+                        </RightBtn>
+                        <SearchContainer>
+                            <Ul>
+                                {books?.map((a, i) => (
+                                    <Item key={i} title={a.title} thumbnail={a.thumbnail} authors={a.authors[0]} />
+                                ))}
+                            </Ul>
+                        </SearchContainer>
+                    </Main>
+                </Container>
+            </TopContainer>
         </>
     );
 };
