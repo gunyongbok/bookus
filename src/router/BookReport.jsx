e1ea5691fe94beb2b03d2fbbf944a6ea8ec7c4cf
gunyongbok <qhrrjsdyd123@gmail.com>
2023-02-17 18:09:22 +0900

    design: 독서록 작성 페이지 디자인 완료

--- src/router/BookReport.jsx
+++ src/router/BookReport.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 import { CgProfile } from 'react-icons/cg';
+import { BsList } from 'react-icons/bs';
+import { AiOutlineSave } from 'react-icons/ai';
 
 const TopContainer = styled.div`
     display: flex;
@@ -26,7 +28,7 @@ const Header = styled.header`
     font-size: 35px;
     font-weight: 250;
     position: absolute;
-    top: 50px;
+    top: 10px;
     right: 15px;
 `;
 
@@ -34,7 +36,7 @@ const Profile = styled.div`
     font-size: 30px;
     position: absolute;
     right: 50px;
-    top: 70px;
+    top: 30px;
 `;
 
 const Main = styled.div`
@@ -42,14 +44,13 @@ const Main = styled.div`
     width: 1100px;
     height: 480px;
     position: absolute;
-
     top: 130px;
     display: flex;
 `;
 
 const BookInfo = styled.div`
     width: 20%;
-    height: 60%;
+    height: 70%;
     display: flex;
     flex-direction: column;
     justify-content: center;
@@ -57,24 +58,71 @@ const BookInfo = styled.div`
 `;
 
 const BookTitleAuthor = styled.div`
-    width: 60%;
-    padding-bottom: 5px;
-    border-bottom: 1px solid black;
+    position: relative;
+    left: 0px;
+    width: 67%;
+    height: 30%;
+    margin-top: 10px;
+    padding-bottom: 10px;
+    border-bottom: 1px solid gray;
+    border-top: 2px solid black;
 `;
 
 const BookReportListContainer = styled.div`
     width: 75%;
-    height: 100%;
+    height: 90%;
+    position: relative;
+    top: 10px;
+`;
+
+const PageReportContainer = styled.div`
+    width: 100%;
+    position: absolute;
+    bottom: 10px;
+    font-size: 13px;
+`;
+
+const StartPage = styled.input`
+    width: 15%;
+    border: none;
+    font-size: 12px;
+    font-weight: 300;
+    text-align: center;
+    &::placeholder,
+    :focus {
+        font-size: 12px;
+        font-weight: 300;
+        outline: none;
+    }
+`;
+
+const EndPage = styled.input`
+    width: 15%;
+    border: none;
+    font-size: 12px;
+    font-weight: 300;
+    text-align: center;
+    &::placeholder,
+    :focus {
+        font-size: 12px;
+        font-weight: 300;
+        outline: none;
+    }
 `;
 
 const TitleInput = styled.input`
-    width: 100%;
+    padding-bottom: 5px;
+    width: 88%;
     height: 10%;
     border: none;
     border-bottom: 1px solid gray;
-    &::placeholder {
-        font-size: 20px;
-        font-weight: 500;
+    font-size: 25px;
+    font-weight: 600;
+    &::placeholder,
+    :focus {
+        font-size: 25px;
+        font-weight: 600;
+        outline: none;
     }
 `;
 
@@ -83,10 +131,56 @@ const ContentsInput = styled.textarea`
     height: 85%;
     border: none;
     margin-top: 15px;
-    &::placeholder {
-        font-size: 10px;
+    font-size: 15px;
+    font-weight: 400;
+    &::placeholder,
+    :focus {
+        font-size: 15px;
         font-weight: 400;
+        outline: none;
     }
+`;
+
+const DateLabel = styled.label`
+    padding-bottom: 5px;
+    width: 5%;
+    height: 10%;
+    border: none;
+    border-bottom: 1px solid gray;
+    font-size: 15px;
+    font-weight: 600;
+    position: relative;
+    top: 12px;
+`;
+
+const DateInput = styled.input`
+    padding-bottom: 6px;
+    width: 9%;
+    height: 10%;
+    border: none;
+    border-bottom: 1px solid gray;
+    font-size: 15px;
+    font-weight: 300;
+    position: relative;
+    top: 11px;
+    &::placeholder,
+    :focus {
+        font-size: 15px;
+        font-weight: 300;
+        outline: none;
+    }
+`;
+
+const InputContainer = styled.div`
+    display: flex;
+`;
+
+const FooterBar = styled.footer`
+    height: 10%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 25px;
 `;
 
 const BookReport = () => {
@@ -102,15 +196,27 @@ const BookReport = () => {
                     </Profile>
                     <Main>
                         <BookInfo>
-                            <img src={books.thumbnail} alt={books.thumbnail} style={{ width: '135px', height: '190px' }} />
+                            <img src={books.thumbnail} alt={books.thumbnail} style={{ width: '145px', height: '190px' }} />
                             <BookTitleAuthor>
                                 <h3 style={{ fontSize: '12px', fontWeight: '700', marginBottom: '5px' }}>{books.title}</h3>
                                 <h4 style={{ fontSize: '10px', fontWeight: '400', margin: '0' }}>{books.authors}</h4>
+                                <PageReportContainer>
+                                    Page
+                                    <StartPage placeholder="000" />-<EndPage placeholder="000" /> p
+                                </PageReportContainer>
                             </BookTitleAuthor>
                         </BookInfo>
                         <BookReportListContainer>
-                            <TitleInput placeholder="제목을 입력하세요."></TitleInput>
+                            <InputContainer>
+                                <TitleInput placeholder="제목을 입력하세요."></TitleInput>
+                                <DateLabel for="date">Date</DateLabel>
+                                <DateInput id="date" placeholder="00. 00. 00"></DateInput>
+                            </InputContainer>
                             <ContentsInput placeholder="내용을 입력하세요."></ContentsInput>
+                            <FooterBar>
+                                <BsList />
+                                <AiOutlineSave />
+                            </FooterBar>
                         </BookReportListContainer>
                     </Main>
                 </Container>
